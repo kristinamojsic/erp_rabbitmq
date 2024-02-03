bc04079a05f2802bfd04e2de93b86eeab691f1ca
kristinamojsic <kmojsic00@gmail.com>
2024-02-03 21:41:22 +0100

    update

--- src/main/java/com/example/parabbitmq/data/Product.java
+++ src/main/java/com/example/parabbitmq/data/Product.java
@@ -9,6 +9,7 @@ public class Product {
     private String productName;
     private double purchasePrice;
     private String measureUnit;
+    private int quantity = 0;
 
     public Product() {
     }
@@ -17,6 +18,13 @@ public class Product {
         this.productName = productName;
         this.purchasePrice = purchasePrice;
         this.measureUnit = measureUnit;
+    }
+
+    public Product(String productName, double purchasePrice, String measureUnit, int quantity) {
+        this.productName = productName;
+        this.purchasePrice = purchasePrice;
+        this.measureUnit = measureUnit;
+        this.quantity = quantity;
     }
 
     public Product(long id, String productName, double purchasePrice, String measureUnit) {
@@ -56,5 +64,26 @@ public class Product {
 
     public void setPurchasePrice(double purchasePrice) {
         this.purchasePrice = purchasePrice;
+    }
+
+    public void updateState(Product product)
+    {
+        this.productName = product.productName;
+        this.purchasePrice = product.purchasePrice;
+        this.measureUnit = product.measureUnit;
+        this.quantity = product.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", purchasePrice=" + purchasePrice +
+                ", measureUnit='" + measureUnit + '\'' +
+                '}';
     }
 }

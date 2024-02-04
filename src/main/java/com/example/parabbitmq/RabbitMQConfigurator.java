2d72e4c6053470907a10dd4c0a99d382074ce8e0
kristinamojsic <kmojsic00@gmail.com>
2024-02-04 17:52:35 +0100

    dodat converter

--- src/main/java/com/example/parabbitmq/RabbitMQConfigurator.java
+++ src/main/java/com/example/parabbitmq/RabbitMQConfigurator.java
@@ -9,11 +9,14 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabbitMQConfigurator {
+public class RabbitMQConfigurator{
     public static final String PRODUCTS_TOPIC_EXCHANGE_NAME = "products-events-exchange";
 
     public static final String PRODUCTS_SERVICE_QUEUE = "products-service-queue";
@@ -43,8 +46,22 @@ public class RabbitMQConfigurator {
         return container;
     }
 
+
     @Bean
-    MessageListenerAdapter listenerAdapter(MessagingReportingService receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
+    MessageListenerAdapter listenerAdapter(MessagingReportingService receiver,MessageConverter messageConverter) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(receiver, "receiveMessage");
+        adapter.setMessageConverter(messageConverter);
+        return adapter;
     }
+
+
+    @Bean
+    public MessageConverter jsonToMapMessageConverter() {
+        DefaultClassMapper defaultClassMapper = new DefaultClassMapper();
+        defaultClassMapper.setTrustedPackages("*");
+        Jackson2JsonMessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        jackson2JsonMessageConverter.setClassMapper(defaultClassMapper);
+        return jackson2JsonMessageConverter;
+    }
+
 }
